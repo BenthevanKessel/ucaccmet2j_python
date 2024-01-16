1acaf16ce958dad230fc999dac4bdfcd0f7ff77d
Benthe van Kessel <benthe.vankessel@gmail.com>
2024-01-16 14:11:24 +0100

    end of excercise 1

--- results.py
+++ results.py
@@ -1,7 +1,8 @@
+#seattle,WA,GHCND:US1WAKG0038
 import json
 import csv
 
-#seattle,WA,GHCND:US1WAKG0038
+# Use this station code to select all the measurements belonging to it from the JSON data.
 seattle_measurement = []
 
 with open('precipitation.json') as file:
@@ -11,4 +12,23 @@ for entry in precipitation:
     if entry['station'] == 'GHCND:US1WAKG0038':
         seattle_measurement.append(entry)
 
-print(seattle_measurement)
+#print(seattle_measurement)
+
+# Calculate the total_monthly_precipitation: a list with the total precipitation per month, for that location
+total_monthly_precipitation = {} #month:value,month:value
+
+for entry in seattle_measurement:
+    date_parts = entry['date'].split('-')
+    month = date_parts[1]
+    if month in total_monthly_precipitation:
+        #month = old value + new value
+        total_monthly_precipitation[month] = entry['value'] + total_monthly_precipitation[month]
+    else:
+        #month = new value
+        total_monthly_precipitation[month] = entry['value']
+
+print(total_monthly_precipitation)
+
+with open('results.json', 'w') as file:
+    json.dump(total_monthly_precipitation, file)
+
