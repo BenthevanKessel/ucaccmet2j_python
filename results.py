200b3ef83455f72947a3e201d49b59b47163aaea
Benthe van Kessel <benthe.vankessel@gmail.com>
2024-01-16 14:36:12 +0100

    end of excercise 2

--- results.py
+++ results.py
@@ -27,8 +27,34 @@ for entry in seattle_measurement:
         #month = new value
         total_monthly_precipitation[month] = entry['value']
 
-print(total_monthly_precipitation)
+# print(total_monthly_precipitation)
 
 with open('results.json', 'w') as file:
     json.dump(total_monthly_precipitation, file)
 
+# Calculate the total_yearly_precipitation, i.e., the sum of the precipitation over the whole year for one location.
+
+with open('results.json') as file:
+    results = json.load(file)
+
+total_yearly_preciptation = 0
+
+for month in results:
+    total_yearly_preciptation = total_yearly_preciptation + results[month]
+
+# print(total_yearly_preciptation)
+
+# Calculate the relative_monthly_precipitation (proportion of the yearly rain) per month, 
+    # e.g., if 20% of the rain in 2010 in Seattle fell in November, the corresponding value should read 0.2
+
+relative_monthly_precipitation = {}
+
+for month in results:
+    relative_monthly_precipitation[month] = results[month]/total_yearly_preciptation
+
+print(relative_monthly_precipitation)
+
+# Commit your script and your output results.json to your version history.
+
+with open('results.json', 'w') as file:
+    json.dump(relative_monthly_precipitation, file)
